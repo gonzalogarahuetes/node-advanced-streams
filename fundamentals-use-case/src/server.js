601f7038655d56bd6a831906b247e85454a99eca
Gonzalo García Arahuetes <gonzaloarahuetes@gmail.com>
2025-08-12 16:13:57 +0200

    add broadcast stream

--- fundamentals-use-case/src/server.js
+++ fundamentals-use-case/src/server.js
@@ -1,10 +1,30 @@
 import { randomUUID } from "node:crypto";
 import net from "node:net";
+import { Writable } from "node:stream";
 
 const clients = new Map();
 
+const broadcastToClient = (senderSocketId, data) => {
+    [...clients.values()]
+    .filter(clientSocket => clientSocket.id !== senderSocketId)
+    .forEach(clientSocket => clientSocket.write(data));
+}
+
+const broadcastStream = (socket) => {
+    return new Writable({
+        write(chunk, encoding, callback) {
+            const data = JSON.stringify({
+                id: socket.id.slice(0, 5),
+                message: chunk.toString()
+            })
+            broadcastToClient(socket.id, data);
+            callback(null, chunk);
+        }
+    })
+}
+
 const server = net.createServer((socket) => {
-    socket.pipe(socket);
+    socket.pipe(broadcastStream(socket));
 });
 
 server.listen(3000, () => "Server is listening on port 3000");
