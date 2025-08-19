07d44bee49c30c2c702eeb446df058ed279fad30
Gonzalo García Arahuetes <gonzaloarahuetes@gmail.com>
2025-08-19 11:26:38 +0200

    transform chunks into one-lined JSON

--- main-use-case-frontend/index.js
+++ main-use-case-frontend/index.js
@@ -3,17 +3,51 @@ const URL = "http://localhost:3000";
 async function consumeAPI(signal) {
     const response = await fetch(URL, { signal });
 
-    const readerObj = response.body.pipeTo(new WritableStream({
-        write(chunk) {
-            console.log("chunk: ", chunk);
-        }
-    }));
+    const readerObj = response.body.pipeThrough(new TextDecoderStream()).pipeThrough(parseChunks())
+
+
 
     return readerObj;
+}
+
+function parseChunks() {
+    let buffer = "";
+    return new TransformStream({
+        transform(chunk, ctrl) {
+            buffer += chunk;
+
+            const lines = buffer.split("\n");
+            buffer = lines.pop();
+
+            for (const line of lines) {
+                try {
+                    ctrl.enqueue(JSON.parse(line));
+                } catch (error) {
+                    console.error("Failed to parse JSON: ", line, error);
+                }
+            }
+        },
+
+        flush(ctrl) {
+            if(buffer.trim()) {
+                try {
+                    ctrl.enqueue(JSON.parse(buffer));
+                } catch (error) {
+                    console.error("Failed to parse JSON during flush: ", buffer, error);
+                }
+            }
+        }
+    })
 }
 
 let abortController = new AbortController();
 
 (async () => {
-    await consumeAPI(abortController.signal);
+   const readableStream = await consumeAPI(abortController.signal);
+   readableStream.pipeTo(new WritableStream({
+        write(chunk) {
+            console.log("chunk: ", chunk);
+            
+        }
+   }))
 })();
